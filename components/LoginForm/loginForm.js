f6fec1fcfa9ed6aeb47ca418eccb8408ed842257
thomasghebray <89873941+thomasghebray@users.noreply.github.com>
2023-04-02 04:03:50 -0500

    Updated Login Page

--- components/LoginForm/loginForm.js
+++ components/LoginForm/loginForm.js
@@ -1,90 +1,121 @@
-import { PrismaClient } from "@prisma/client";
-import { initFirebase } from '@/lib/firebaseApp';
-import { useRouter } from 'next/router';
-import React, { useState } from 'react';
-import { Button, Form } from 'react-bootstrap';
-import { useAuth } from '../../context/AuthContext';
-
+import { initFirebase } from "@/lib/firebaseApp";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import { useAuth } from "../../context/AuthContext";
 
 export default function LoginForm() {
-  //initializing configuration
+  // Initialize Firebase
   initFirebase();
-  
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
   const router = useRouter();
-  const { signIN, googleSignIn } = useAuth();
+  const { signIn, googleSignIn } = useAuth();
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    signIN(email, password)
-      .then(authUser => {
-        console.log("Success. The user is created in Firebase")
-        router.push("/dashboard");
-      })
-      .catch(error => {
-        console.log(error)
-        alert("Email/Password combination is incorrect.")
-        // An error occurred. Set error message to be displayed to user
-        setError(error.message)
-      });
-  }
-
+    try {
+      await signIn(email, password);
+      router.push("/dashboard");
+    } catch (error) {
+      console.log(error);
+      setError("Email/Password combination is incorrect.");
+    }
+  };
 
   const signInWithGoogle = async () => {
-    const result = await googleSignIn()
-    router.push("/dashboard")
-  }
+    try {
+      await googleSignIn();
+      router.push("/dashboard");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  const goToSignUp = async () => {
+  const goToSignUp = () => {
     router.push("/signUp");
-
-  }
-
+  };
 
   return (
-    <div>
-       <div
-      style={{
-        width: '40%',
-        margin: 'auto',
-      }}
-    >
-      <h1 className="text-center my-3 font-weight-bold">Login</h1>
+    <div className="container my-5 py-5">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card shadow-lg border-0 rounded-lg mt-5">
+            <div className="card-header">
+              <h3
+                className="text-center"
+                style={{
+                  fontFamily: "cursive",
+                  fontWeight: "600",
+                  fontSize: "3rem",
+                  marginBottom: "1rem",
+                }}
+              >
+                My Golden Nails N' Facials
+              </h3>
+            </div>
+            <div className="card-body">
+              <Form onSubmit={handleLogin}>
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control
+                    type="email"
+                    placeholder="Enter email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
+                </Form.Group>
 
-      <Form onSubmit={handleLogin}>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            onChange={(event) => setEmail(event.target.value)}
-            required
-            type="email"
-            placeholder="Enter email"
-          />
-        </Form.Group>
+                <Form.Group controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            onChange={(event) => setPassword(event.target.value)}
-            required
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Login
-        </Button>
-        <br></br>
-        <br></br>
-      </Form>
-      <Button onClick={() => goToSignUp()}>Create Account</Button>
-      <br></br>
-      <br/>
-      <Button onClick={() => signInWithGoogle()}> Sign in with Google</Button>
+                {error && <p className="text-danger mb-3">{error}</p>}
 
-    </div>
+                <Button
+                  variant="primary"
+                  type="submit"
+                  className="btn-block"
+                  style={{ backgroundColor: "#007bff" }}
+                >
+                  Login
+                </Button>
+              </Form>
+            </div>
+            <div className="card-footer text-center">
+              <div className="small">
+                Don't have an account?{" "}
+                <Button
+                  variant="success"
+                  className="p-0 m-0"
+                  onClick={goToSignUp}
+                >
+                  Sign Up Now
+                </Button>
+              </div>
+              <hr className="my-2" />
+              <div className="text-center">
+                <Button
+                  variant="outline-primary"
+                  onClick={signInWithGoogle}
+                >
+                  Sign in with Google
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
