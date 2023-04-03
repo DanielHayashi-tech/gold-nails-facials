bbd2ec2d2778d554f87797705651f52cf3bb51d8
Saeed Rajan <sr4945@gmail.com>
2023-04-03 14:31:59 -0500

    removed sign in with google

--- pages/api/dashboard.js
+++ pages/api/dashboard.js
@@ -3,26 +3,6 @@ import { authMiddleware } from '../../lib/authMiddleware';
 import jwt from 'jsonwebtoken';
 
 
-
-
-// export default function handler(req, res) {
-//   const authHeader = req.headers.authorization;
-//   if (!authHeader || !authHeader.startsWith('Bearer ')) {
-//     return res.status(401).json({ message: 'Unauthorized' });
-//   }
-//   const token = authHeader.split(' ')[1];
-//   try {
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//     req.user = decoded;
-//     next()
-//   } catch (error) {
-//     return res.status(401).json({ message: 'Invalid token' });
-//   }
-//   console.log("test")
-//   return res.status(405).json({ message: 'Method not allowed' });
-  
-//   }
-
 export default async function handler(req, res) {
   if (req.method === 'GET') {
     authMiddleware(req, res, async () => {
