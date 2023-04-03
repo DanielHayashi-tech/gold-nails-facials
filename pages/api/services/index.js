7de423f5c3aa6c530c16cb7d6001b148b7ac2109
Saeed Rajan <sr4945@gmail.com>
2023-04-03 16:24:48 -0500

    added rough code for creating a new order

--- pages/api/services/index.js
+++ pages/api/services/index.js
@@ -7,6 +7,15 @@ export default async function handler(req, res) {
   if (req.method === 'POST') {
     authMiddleware(req, res, async () => {
       //prsima here
+
+        //client posts what services are needed
+
+        //get prices for services
+
+        //create new order with clientid, total price and date
+
+
+        //create order service line for each of services with orderid and serviceid
     });
   } else {
     res.status(405).json({ message: 'Method not allowed' });
