769e1887ba650dc18e3cc9fc2a7d0f230559400a
Danny Hayashi <72723716+DanielHayashi-tech@users.noreply.github.com>
2023-04-02 20:04:35 -0500

    added Navbar to Login page

---
+++ pages/api/loginlinks.js
@@ -0,0 +1,7 @@
+import { IoHome } from "react-icons/io5";
+import { FaEnvelope } from "react-icons/fa";
+import { BsHandbagFill } from "react-icons/bs";
+import { BsPatchExclamationFill } from "react-icons/bs";
+
+
+export const loginlinks = [];
