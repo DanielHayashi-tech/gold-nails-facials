57ec648039489fe6cbd5809f8df15f022668d680
Danny Hayashi <72723716+DanielHayashi-tech@users.noreply.github.com>
2023-04-04 00:36:35 -0500

    dummy commit

--- components/Nav/LoginNavbar.js
+++ components/Nav/LoginNavbar.js
@@ -1,16 +1,7 @@
-import {
-    Box,
-    Flex,
-    IconButton,
-    useColorModeValue,
-    Icon,
-  } from "@chakra-ui/react";
-  import { Lobster_Two } from "next/font/google";
-  import React from "react";
-  import { Link } from "react-scroll";
-  import { loginlinks } from "@/pages/api/loginlinks";
-  import { FaAlignJustify } from "react-icons/fa";
-  
+import {Flex } from "@chakra-ui/react";
+import { Lobster_Two } from "next/font/google";
+import React from "react";
+
   
   const lobsterTwo = Lobster_Two({
     weight: ["400", "400"],
@@ -19,11 +10,11 @@ import {
   
   
   const LoginNavbar = React.forwardRef((props, ref) => {
-    const navBg = useColorModeValue("white", "gray.400");
+
   
   
     return (
-      <Flex top="0" w="100%" zIndex="sticky" bg={navBg} position="fixed" ref={ref}>
+      <Flex top="0" w="100%" zIndex="sticky" position="fixed" ref={ref}>
         <div className="text-[22px] container flex items-center justify-between px-1 py-3 mx-auto">
           <div className={`text-[34px] font-bold ${lobsterTwo.className}`}>Golden Nails n Facial</div>
           <div className="hidden gap-10 md:flex">
