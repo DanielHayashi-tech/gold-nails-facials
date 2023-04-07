4111eb2370f07ca7bb3d339b6c467fa11b18eaae
Danny Hayashi <72723716+DanielHayashi-tech@users.noreply.github.com>
2023-04-06 21:54:40 -0500

    try to find my comment

--- components/ActiveLink/activelink.js
+++ components/ActiveLink/activelink.js
@@ -1,5 +1,8 @@
 import { useRouter } from 'next/router'
 
+
+//Here is a comment I want you to find it and remove it -- fro you thomas
+
 function ActiveLink({ href, children }) {
   const router = useRouter()
   
