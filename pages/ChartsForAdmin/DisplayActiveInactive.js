9f4e533c48765e913255b4fc216b4e2740941968
Danny Hayashi <72723716+DanielHayashi-tech@users.noreply.github.com>
2023-04-11 03:25:34 -0500

    No erros in the console and getting the right data back plz help

---
+++ pages/ChartsForAdmin/DisplayActiveInactive.js
@@ -0,0 +1,105 @@
+import React, { useEffect, useState } from 'react';
+import { useAuth } from '../../context/AuthContext';
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts';
+
+const DisplayActiveInactive = () => {
+  const { getToken } = useAuth();
+
+  const [chartData, setChartData] = useState(null);
+
+  const updateClientStatus = async () => {
+    try {
+      const token = await getToken();
+
+      const response = await fetch('/api/admin/updateClientStatus', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+
+      const data = await response.json();
+      console.log("updateClientStatus data:", data);
+
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchChartData = async () => {
+    try {
+      const token = await getToken();
+      console.log("token:", token)
+
+      const response = await fetch('/api/admin/getClientStatusCounts', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      console.log("response:", response)
+
+      const data = await response.json();
+      console.log("Chart data:", data);
+      setChartData(data);
+    } catch (error) {
+      console.log(error);
+      alert('Read the error message and try again, you got this :) ');
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const token = await getToken();
+      if (token) {
+        await updateClientStatus();
+        await fetchChartData();
+      }
+    };
+    fetchData();
+  }, [getToken]);
+
+  return (
+    <div className="flex">
+    {chartData && (
+      <div className="flex">
+        <div style={{ width: '100%', height: 300 }}>
+          <ResponsiveContainer>
+            <BarChart data={chartData}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="timeframe" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="activeClients" fill="#8884d8" />
+              <Bar dataKey="inactiveClients" fill="#82ca9d" />
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
+      </div>
+    )}
+  </div>
+  );
+};
+
+export default DisplayActiveInactive;
--- pages/adminDash/index.js
+++ pages/adminDash/index.js
@@ -10,24 +10,18 @@ import AddEmployeeForm from '../../components/AdminForms/AddEmployeeForm';
 import UpdateEmpForm from '../../components/AdminForms/updateEmpForm';
 
 import Link from 'next/link';
-// import DisplaySales from '../ChartsForAdmin/DisplaySales';
 import DisplayServices from '../ChartsForAdmin/DisplayServices';
-// import DisplayActiveInactive from '../ChartsForAdmin/DisplayActive-Inactive';
+import DisplayActiveInactive from '../ChartsForAdmin/DisplayActiveInactive';
 
- 
- 
+
 export default function AdminDash() {
     const { getToken } = useAuth();
 
     const router = useRouter();
-    const [addEmp, setAddEmp] = useState(false);
     const [error, setError] = useState(null);
-    const [ServiceID, setServiceID] = useState("");
-    const [service_price, setServicePrice] = useState("");
     const [serviceOrderCount, setServiceOrderCount] = useState(0);
     const [clientCount, setclientCount] = useState(0);
     const [currentForm, setCurrentForm] = useState('');
-    const [serviceTypes, setServiceTypes] = useState([]);
 
 
 
@@ -79,115 +73,112 @@ export default function AdminDash() {
     };
 
 
-  const handleFormChange = (form) => {
-      setCurrentForm(form);
-  };
+    const handleFormChange = (form) => {
+        setCurrentForm(form);
+    };
 
-  const handleCancelForm = () => {
-      setCurrentForm('');
-  };
- 
-
-  return (
-      <div>
-
-        <AdminDashNavbar handleFormChange={handleFormChange} />
-        {currentForm === 'addEmployee' && (
-            <AddEmployeeForm handleCancelForm={handleCancelForm} />
-        )}
-        {currentForm === 'updateServicePrice' && (
-            <UpdateServicePriceForm handleCancelForm={handleCancelForm} />
-        )}
-        {currentForm === 'updateEmployee' && (
-            <UpdateEmpForm handleCancelForm={handleCancelForm} />
-        )}
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        
-        <div className="charts-container grid grid-cols-3 gap-4 align-content-center">
-          <div className="text-center">
-          </div>
-          <div className="text-center">
-          </div>
-          <div className="text-center">
-          </div>
-          
-          <DisplayServices />
-
-          <div>
-          </div>
-          
+    const handleCancelForm = () => {
+        setCurrentForm('');
+    };
 
 
-          {/* <div className="col-span-2 text-center">
-            <div className="chart-container">
-                <DisplaySales />
+    return (
+        <div>
+
+            <AdminDashNavbar handleFormChange={handleFormChange} />
+            {currentForm === 'addEmployee' && (
+                <AddEmployeeForm handleCancelForm={handleCancelForm} />
+            )}
+            {currentForm === 'updateServicePrice' && (
+                <UpdateServicePriceForm handleCancelForm={handleCancelForm} />
+            )}
+            {currentForm === 'updateEmployee' && (
+                <UpdateEmpForm handleCancelForm={handleCancelForm} />
+            )}
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+
+            <div className="charts-container grid grid-cols-4 gap-4 align-content-center">
+                <div className="text-center">
+                </div>
+                <div className="text-center">
+                </div>
+                <div className="text-center">
+                </div>
+
+                <div className="col-span-1 text-center">
+                    <div className="chart-container">
+                        <DisplayServices />
+                    </div>
+                </div>
+
+                <div className="col-span-1 text-center">
+                    <div className="chart-container">
+                        <DisplayActiveInactive />
+                    </div>
+                </div>
+
+
+
+                {/* <div className="col-span-2 text-center">
+                    <div className="chart-container">
+                        <DisplayActiveInactive />
+                    </div>
+                </div> */}
+
+
+
+                <div className="col-span-1 text-center">
+                    <div className="chart-container">
+                        <Form onSubmit={handleServiceTotal}>
+                            <p>Total Service Orders: {serviceOrderCount}</p>
+                            <Button
+                                variant="primary"
+                                type="submit"
+                                className="btn-block custom-button cursor-pointer hover:text-pink-900"
+                                style={{ backgroundColor: "#ffe5e9", fontSize: "1rem" }}>
+                                Total the Service Orders!
+                            </Button>
+                        </Form>
+                    </div>
+                </div>
+
+                <div className="text-center">
+                    Owner needs to be able to approve quotes and
+                    this means change the status to IN PROGRSS and display how many
+                </div>
+
+                <div className="text-center">
+                </div>
+
+                <div className="col-span-1 text-center">
+                    <div className="chart-container">
+                        <Form onSubmit={handleCountAllMyClients}>
+                            <p>Total Clients: {clientCount}</p>
+                            <Button
+                                variant="primary"
+                                type="submit"
+                                className="btn-block custom-button cursor-pointer hover:text-pink-900"
+                                style={{ backgroundColor: "#ffe5e9", fontSize: "1rem" }}>
+                                Total Number of Clients!
+                            </Button>
+                        </Form>
+                    </div>
+                </div>
+                <br></br>
+
+
+                <div className="text-center">
+                    Sales on Services Made Today, this week, this month, this year
+                </div>
+
             </div>
-          </div>
-
-          <div className="col-span-2 text-center">
-            <div className="chart-container">
-                <DisplayActiveInactive />
-            </div>
-          </div> */}
-
-          <div className="col-span-1 text-center">
-          <div className="text-center">
-            {/* Owner needs to be able to approve quotes and
-            this means change the status to IN PROGRSS and display how many */}
-          </div>
-          </div>
-
-
-          <div className="col-span-1 text-center">
-            <div className="chart-container">
-                <Form onSubmit={handleServiceTotal}>
-                    <p>Total Service Orders: {serviceOrderCount}</p>
-                    <Button
-                    variant="primary"
-                    type="submit"
-                    className="btn-block custom-button cursor-pointer hover:text-pink-900"
-                    style={{ backgroundColor: "#ffe5e9", fontSize: "1rem" }}>
-                    Total the Service Orders!
-                    </Button>
-                </Form>
-            </div>
-          </div>
-
-          <div className="text-center">
-            Owner needs to be able to approve quotes and
-            this means change the status to IN PROGRSS and display how many
-          </div>
-
-          <div className="col-span-1 text-center">
-            <div className="chart-container">
-                <Form onSubmit={handleCountAllMyClients}>
-                    <p>Total Clients: {clientCount}</p>
-                    <Button
-                    variant="primary"
-                    type="submit"
-                    className="btn-block custom-button cursor-pointer hover:text-pink-900"
-                    style={{ backgroundColor: "#ffe5e9", fontSize: "1rem" }}>
-                    Total Number of Clients!
-                    </Button>
-                </Form>
-            </div>
-          </div>
-          <br></br>
-
-
-          <div className="text-center">
-            Sales on Services Made Today, this week, this month, this year
-          </div>
-    
-          </div>
-      </div>
-  );
+        </div>
+    );
 }
 
- 
- 
+
