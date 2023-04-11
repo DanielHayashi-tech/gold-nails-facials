b39947547d323127f43d25d62cafe513b0741dfb
Danny Hayashi <72723716+DanielHayashi-tech@users.noreply.github.com>
2023-04-11 08:12:00 -0500

    wrapped admin Dash component in a container and added color to AdminDashNavbar

--- components/Nav/AdminDashNavbar.js
+++ components/Nav/AdminDashNavbar.js
@@ -1,4 +1,4 @@
-import { Box, Flex, IconButton, useColorModeValue, Icon, Button } from '@chakra-ui/react';
+import { Box, Flex, useColorModeValue, VStack } from '@chakra-ui/react';
 import { Raleway } from 'next/font/google';
 import React from 'react';
 
@@ -8,21 +8,52 @@ const raleway = Raleway({
 });
 
 const AdminDashNavBar = ({ handleFormChange }) => {
-  const navBg = useColorModeValue('#faf3f7', 'gray.400');
+  const navBg = useColorModeValue('#abb1cf', 'gray.400');
+
+  const buttonStyles = {
+    as: 'button',
+    height: '0px',
+    lineHeight: '1',
+    transition: 'all 0.2s cubic-bezier(4.1,3.2,4.52,1)',
+    border: '1px',
+    px: '25px',
+    borderRadius: '2px',
+    fontSize: '20px',
+    fontWeight: 'semibold',
+    borderColor: '#cc7eb7',
+    color: '#4b4f56',
+    _hover: { bg: 'pink.500' },
+    _active: {
+      bg: '#e185e6',
+      transform: 'scale(0.98)',
+      borderColor: '#bec3c9',
+    },
+    _focus: {
+      boxShadow:
+        '0 0 1px 2px rgba(88, 144, 255, .75), 0 1px 1px rgba(0, 0, 0, .15)',
+    },
+  };
 
   return (
-    <Flex top="0" w="100%" zIndex="sticky" bg={navBg} position="fixed">
-      <div className="text-[22px] container flex items-center justify-between px-12 py-4 mx-auto">
+    <VStack spacing={4} top="0" w="100%" zIndex="sticky" bg={navBg} position="fixed">
+      <div className="text-[22px] container flex items-center ">
         <div className={`text-[35px] font-bold ${raleway.className}`}>Golden Nails n Facial</div>
-
-        <div className="flex space-x-4">
-          <Button onClick={() => handleFormChange('addEmployee')}>Add a new Employee</Button>
-          <Button onClick={() => handleFormChange('updateEmployee')}>Update Employee </Button>
-          <Button onClick={() => handleFormChange('updateEmployeeSkill')}>Update Employee Skill</Button>
-          <Button onClick={() => handleFormChange('updateServicePrice')}>Update Service Price</Button>
-        </div>
       </div>
-    </Flex>
+      <Flex className="container flex items-end justify-between px-12 py-4 mx-auto" wrap="wrap">
+        <Box {...buttonStyles} bg="#f5f6f7" onClick={() => handleFormChange('addEmployee')}>
+          Add a new Employee
+        </Box>
+        <Box {...buttonStyles} bg="#f5f6f7" onClick={() => handleFormChange('updateEmployee')}>
+          Update Employee
+        </Box>
+        <Box {...buttonStyles} bg="#f5f6f7" onClick={() => handleFormChange('updateEmployeeSkill')}>
+          Update Employee Skill
+        </Box>
+        <Box {...buttonStyles} bg="#f5f6f7" onClick={() => handleFormChange('updateServicePrice')}>
+          Update Service Price
+        </Box>
+      </Flex>
+    </VStack>
   );
 };
 
