7b89ae4bf3fa20e2853a27d38967564f57014037
thomasghebray <89873941+thomasghebray@users.noreply.github.com>
2023-04-12 14:27:04 -0500

    Labels For Charts

--- pages/adminDash/index.js
+++ pages/adminDash/index.js
@@ -129,67 +129,73 @@ export default function AdminDash() {
             <br></br>
 
             <div className="mt-20">
-                <div className="charts-container grid grid-cols-3 gap-4">
-                    <div className="col-span-1 text-center">
-                        <div className="p-4 bg-gray-100 rounded-lg shadow-md">
-                            <DisplayServices />
-                        </div>
-                    </div>
-                    <div className="col-span-1 text-center">
-                        <div className="p-4 bg-gray-100 rounded-lg shadow-md">
-                            
-                        </div>
-                    </div>
-                    <div className="col-span-1 text-center">
-                        <div className="p-4 bg-gray-100 rounded-lg shadow-md">
-                            <DisplayActiveInactive />
-                        </div>
-                    </div>
-                    <div className="col-span-3 text-center">
-                        <div className="p-4 bg-gray-100 rounded-lg shadow-md">
-                            <DisplayServiceTrends  />
+    <div className="charts-container grid grid-cols-3 gap-4">
+        <div className="col-span-1 text-center">
+            <div className="p-4 bg-gray-100 rounded-lg shadow-md">
+                <h2 className="text-lg font-medium mb-2">Service Summary</h2>
+                <DisplayServices />
+            </div>
+        </div>
+        <div className="col-span-1 text-center">
+            <div className="p-4 bg-gray-100 rounded-lg shadow-md">
+                {/* This chart is currently empty */}
+            </div>
+        </div>
+        <div className="col-span-1 text-center">
+            <div className="p-4 bg-gray-100 rounded-lg shadow-md">
+                <h2 className="text-lg font-medium mb-2">Active vs Inactive Services</h2>
+                <DisplayActiveInactive />
+            </div>
+        </div>
+        <div className="col-span-3 text-center">
+            <div className="p-4 bg-gray-100 rounded-lg shadow-md">
+                <h2 className="text-lg font-medium mb-2">Service Trends</h2>
+                <DisplayServiceTrends />
+            </div>
+        </div>
 
-                        </div>
-                    </div>
-                    
-                    <div className="col-span-1 text-center">
-                        <div className="p-4 bg-gray-100 rounded-lg shadow-md">
-                            <Form onSubmit={handleServiceTotal}>
-                                <p>Total Service Orders: {serviceOrderCount}</p>
-                                <Button
-                                    variant="primary"
-                                    type="submit"
-                                    className="btn-block custom-button cursor-pointer hover:text-pink-900"
-                                    style={{ backgroundColor: "#ffe5e9", fontSize: "1rem" }}>
-                                    Total the Service Orders!
-                                </Button>
-                            </Form>
-                        </div>
-                    </div>
-                    <div className="col-span-1 text-center">
-                        <div className="p-4 bg-gray-100 rounded-lg shadow-md">
+        <div className="col-span-1 text-center">
+    <div className="p-4 bg-gray-100 rounded-lg shadow-md">
+        <h2 className="text-lg font-medium mb-2">Service Order Total</h2>
+        <Form onSubmit={handleServiceTotal}>
+            <p>Total Service Orders: {serviceOrderCount}</p>
+            <Button
+                variant="primary"
+                type="submit"
+                className="btn-block custom-button cursor-pointer hover:text-pink-900"
+                style={{ backgroundColor: "#ffe5e9", fontSize: "1rem" }}>
+                Total the Service Orders!
+            </Button>
+        </Form>
+    </div>
+</div>
+<div className="col-span-1 text-center">
+    <div className="p-4 bg-gray-100 rounded-lg shadow-md">
+        {/* This section is currently empty */}
+    </div>
+</div>
+<div className="col-span-1 text-center">
+    <div className="p-4 bg-gray-100 rounded-lg shadow-md">
+        <h2 className="text-lg font-medium mb-2">Client Count</h2>
+        <Form onSubmit={handleCountAllMyClients}>
+            <p>Total Clients: {clientCount}</p>
+            <Button
+                variant="primary"
+                type="submit"
+                className="btn-block custom-button cursor-pointer hover:text-pink-900"
+                style={{ backgroundColor: "#ffe5e9", fontSize: "1rem" }}>
+                Total Number of Clients!
+            </Button>
+        </Form>
+    </div>
+</div>
+<div className="col-span-3 text-center">
+    <div className="p-4 bg-gray-100 rounded-lg shadow-md">
+        <h2 className="text-lg font-medium mb-2">Service Order Details</h2>
+        <DisplayServiceOrders />
+    </div>
+</div>
 
-                        </div>
-                    </div>
-                    <div className="col-span-1 text-center">
-                        <div className="p-4 bg-gray-100 rounded-lg shadow-md">
-                            <Form onSubmit={handleCountAllMyClients}>
-                                <p>Total Clients: {clientCount}</p>
-                                <Button
-                                    variant="primary"
-                                    type="submit"
-                                    className="btn-block custom-button cursor-pointer hover:text-pink-900"
-                                    style={{ backgroundColor: "#ffe5e9", fontSize: "1rem" }}>
-                                    Total Number of Clients!
-                                </Button>
-                            </Form>
-                        </div>
-                    </div>
-                    <div className="col-span-3 text-center">
-                        <div className="p-4 bg-gray-100 rounded-lg shadow-md">
-                            <DisplayServiceOrders />
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
